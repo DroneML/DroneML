332d742364ea0a77cd20acec73c8bb0a926e3b68
Ou Ku <o.ku@esciencecenter.nl>
2024-10-10 13:57:33 +0200

    add plugin build generated template

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 from .droneml import DroneMLPlugin
 
-def classFactory(iface):
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load DroneMLPlugin class from file DroneMLPlugin.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    
     return DroneMLPlugin(iface)
--- droneml_dialog.py
+++ droneml_dialog.py
@@ -1,15 +1,20 @@
+import os
+
+from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
 
-class DroneMLDialog(QtWidgets.QDialog):
+# This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'droneml_dialog_base.ui'))
+
+
+class DroneMLPluginDialog(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
-        super(DroneMLDialog, self).__init__(parent)
+        super(DroneMLPluginDialog, self).__init__(parent)
         # Set up the user interface from Designer through FORM_CLASS.
         # After self.setupUi() you can access any designer object by doing
         # self.<objectname>, and you can use autoconnect slots - see
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
-        # self.setupUi(self)
-
-        self.setWindowTitle("DroneML Plugin")
-        self.resize(500,300)
+        self.setupUi(self)

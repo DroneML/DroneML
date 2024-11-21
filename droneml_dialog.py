693c6eea102033d0f7b75e3673a75f169b1dd1c2
Ou Ku <o.ku@esciencecenter.nl>
2024-11-21 10:44:03 +0100

    read vector layers to geodataframes

--- droneml_dialog.py
+++ droneml_dialog.py
@@ -1,5 +1,7 @@
 from qgis.PyQt import QtWidgets
 from qgis.core import QgsRasterLayer, QgsVectorLayer, QgsProject
+import shapely
+import geopandas as gpd
 
 
 class DroneMLDialog(QtWidgets.QDialog):
@@ -10,7 +12,6 @@ class DroneMLDialog(QtWidgets.QDialog):
         # Set up the dialog window properties
         self.setWindowTitle("DroneML Plugin")
         self.resize(800, 600)
-
 
         # Create a layout to organize widgets in the dialog
         layout = QtWidgets.QVBoxLayout()
@@ -28,23 +29,23 @@ class DroneMLDialog(QtWidgets.QDialog):
         # Combo box for raster layers
         vec_positive_label = QtWidgets.QLabel("Vector layer for positive labels:")
         layout.addWidget(vec_positive_label)
-        self.vec_positive = QtWidgets.QComboBox()
-        _populate_vector_combo(self.vec_positive)
-        layout.addWidget(self.vec_positive)
+        self.vec_positive_combo = QtWidgets.QComboBox()
+        _populate_vector_combo(self.vec_positive_combo)
+        layout.addWidget(self.vec_positive_combo)
 
-         # Combo box for raster layers
+        # Combo box for raster layers
         vec_negative_label = QtWidgets.QLabel("Vector layer for negative labels:")
         layout.addWidget(vec_negative_label)
-        self.vec_negative = QtWidgets.QComboBox()
-        _populate_vector_combo(self.vec_negative)
-        layout.addWidget(self.vec_negative)
+        self.vec_negative_combo = QtWidgets.QComboBox()
+        _populate_vector_combo(self.vec_negative_combo)
+        layout.addWidget(self.vec_negative_combo)
 
         # Create a horizontal layout for buttons (zoom in/out and load raster)
         button_layout = QtWidgets.QHBoxLayout()
 
         # Add Load Raster Button
         run_button = QtWidgets.QPushButton("run")
-        # run_button.clicked.connect(self.load_raster)
+        run_button.clicked.connect(self.run_classification)
         run_button.setFixedSize(64, 32)
         button_layout.addWidget(run_button)
 
@@ -53,6 +54,32 @@ class DroneMLDialog(QtWidgets.QDialog):
 
         # Set the layout to the dialog
         self.setLayout(layout)
+
+    def run_classification(self):
+        """Run the classification algorithm."""
+
+        raster_layer = QgsProject.instance().mapLayersByName(
+            self.raster_combo.currentText()
+        )[0]
+        vec_positive_layer = QgsProject.instance().mapLayersByName(
+            self.vec_positive_combo.currentText()
+        )[0]
+        vec_negative_layer = QgsProject.instance().mapLayersByName(
+            self.vec_negative_combo.currentText()
+        )[0]
+
+        print(f"Raster Layer: {raster_layer}")
+        print(f"Positive Vector Layer: {vec_positive_layer}")
+        print(f"Negative Vector Layer: {vec_negative_layer}")
+
+        # Convert positive vector layer to GeoDataFrame
+        positive_vector_gdf = _qgs_vector_layer_to_gdf(vec_positive_layer)
+        print(positive_vector_gdf)
+
+        # Convert negative vector layer to GeoDataFrame
+        negative_vector_gdf = _qgs_vector_layer_to_gdf(vec_negative_layer)
+        print(negative_vector_gdf)
+
 
 def _populate_raster_combo(raster_combo):
     """Poluate the raster combo box with the loaded raster layers."""
@@ -63,6 +90,7 @@ def _populate_raster_combo(raster_combo):
         if isinstance(layer, QgsRasterLayer):
             raster_combo.addItem(layer.name())
 
+
 def _populate_vector_combo(vector_combo):
     """Poluate the vector combo box with the loaded vector layers."""
 
@@ -72,4 +100,12 @@ def _populate_vector_combo(vector_combo):
         if isinstance(layer, QgsVectorLayer):
             vector_combo.addItem(layer.name())
 
-    
+
+# Convert QgsVectorLayer to GeoPandas DataFrame
+def _qgs_vector_layer_to_gdf(qgs_layer):
+    features = [f for f in qgs_layer.getFeatures()]
+    geometries = [shapely.from_wkt(f.geometry().asWkt()) for f in features]
+    attributes = [f.attributes() for f in features]
+    field_names = [field.name() for field in qgs_layer.fields()]
+    gdf = gpd.GeoDataFrame(attributes, columns=field_names, geometry=geometries)
+    return gdf

1a35bf8a236909cdc7c5902ae8ad5a0ca2827359
Ou Ku <o.ku@esciencecenter.nl>
2025-02-09 11:46:51 +0100

    implement new parametes

--- droneml_dialog.py
+++ droneml_dialog.py
@@ -59,6 +59,67 @@ class DroneMLDialog(QtWidgets.QDialog):
         self._populate_vector_combo(self.vec_negative_combo)
         self.layout.addWidget(self.vec_negative_combo)
 
+        # Radio buttons for feature type
+        self.feature_type_label = QtWidgets.QLabel("Feature type:")
+        self.feature_type_label.setStyleSheet(f"font-size: {FONTSIZE}px;")
+        self.feature_type_label.setFixedSize(600, 15)
+        self.layout.addWidget(self.feature_type_label)
+        self.feature_type_group = QtWidgets.QButtonGroup(self)
+        self.feature_type_layout = QtWidgets.QHBoxLayout()
+        self.feature_type_flair = QtWidgets.QRadioButton("FLAIR")
+        self.feature_type_flair.setChecked(True)
+        self.feature_type_identical = QtWidgets.QRadioButton("IDENTITY")
+        self.feature_type_group.addButton(self.feature_type_flair)
+        self.feature_type_group.addButton(self.feature_type_identical)
+        self.feature_type_layout.addWidget(self.feature_type_flair)
+        self.feature_type_layout.addWidget(self.feature_type_identical)
+        self.layout.addLayout(self.feature_type_layout)
+
+        # Radio buttons for compute mode
+        self.compute_mode_label = QtWidgets.QLabel("Compute mode:")
+        self.compute_mode_label.setStyleSheet(f"font-size: {FONTSIZE}px;")
+        self.compute_mode_label.setFixedSize(600, 15)
+        self.layout.addWidget(self.compute_mode_label)
+        self.compute_mode_group = QtWidgets.QButtonGroup(self)
+        self.compute_mode_layout = QtWidgets.QHBoxLayout()
+        self.compute_mode_normal = QtWidgets.QRadioButton("Normal")
+        self.compute_mode_normal.setChecked(True)
+        self.compute_mode_parallel = QtWidgets.QRadioButton("Parallel")
+        self.compute_mode_safe = QtWidgets.QRadioButton("Safe")
+        self.compute_mode_group.addButton(self.compute_mode_normal)
+        self.compute_mode_group.addButton(self.compute_mode_parallel)
+        self.compute_mode_group.addButton(self.compute_mode_safe)
+        self.compute_mode_layout.addWidget(self.compute_mode_normal)
+        self.compute_mode_layout.addWidget(self.compute_mode_parallel)
+        self.compute_mode_layout.addWidget(self.compute_mode_safe)
+        self.layout.addLayout(self.compute_mode_layout)
+
+        # Advanced Options
+        self.advanced_group_box = QtWidgets.QGroupBox("Advanced Options")
+        self.advanced_group_box.setCheckable(True)
+        self.advanced_group_box.setChecked(False)
+        self.advanced_group_box.setMaximumHeight(200)
+        self.advanced_layout = QtWidgets.QVBoxLayout()
+
+        # Chunk size
+        self.chunk_size_label = QtWidgets.QLabel("Chunk size:")
+        self.chunk_size_spinbox = QtWidgets.QSpinBox()
+        self.chunk_size_spinbox.setRange(250, 10000)
+        self.chunk_size_spinbox.setValue(100)
+        self.advanced_layout.addWidget(self.chunk_size_label)
+        self.advanced_layout.addWidget(self.chunk_size_spinbox)
+
+        # Overlap size
+        self.overlap_size_label = QtWidgets.QLabel("Overlap size:")
+        self.overlap_size_spinbox = QtWidgets.QSpinBox()
+        self.overlap_size_spinbox.setRange(0, 1000)
+        self.overlap_size_spinbox.setValue(25)
+        self.advanced_layout.addWidget(self.overlap_size_label)
+        self.advanced_layout.addWidget(self.overlap_size_spinbox)
+
+        self.advanced_group_box.setLayout(self.advanced_layout)
+        self.layout.addWidget(self.advanced_group_box)
+
         # Create a horizontal layout for buttons (zoom in/out and load raster)
         button_layout = QtWidgets.QHBoxLayout()
 
@@ -94,21 +155,46 @@ class DroneMLDialog(QtWidgets.QDialog):
             .mapLayersByName(self.vec_negative_combo.currentText())[0]
             .source()
         )
-
         print(f"Raster Layer: {raster_path}")
         print(f"Positive Vector Layer: {pos_labels_path}")
         print(f"Negative Vector Layer: {neg_labels_path}")
+
+        # Get Feture Type
+        feature_type = (
+            FeatureType.FLAIR
+            if self.feature_type_flair.isChecked()
+            else FeatureType.IDENTITY
+        )
+        print(f"Feature Type: {feature_type}")
+
+        # Get Compute Mode
+        if self.compute_mode_normal.isChecked():
+            compute_mode = "normal"
+        elif self.compute_mode_parallel.isChecked():
+            compute_mode = "parallel"
+        else:
+            compute_mode = "safe"
+        print(f"Compute Mode: {compute_mode}")
+
+        # Get chunk size and overlap size
+        if self.advanced_group_box.isChecked():
+            chunk_size = self.chunk_size_spinbox.value()
+            overlap_size = self.overlap_size_spinbox.value()
+            print(f"Chunk Size: {chunk_size}")
+            print(f"Overlap Size: {overlap_size}")
+        else:
+            chunk_size = None
+            overlap_size = None
 
         prediction_tif = read_input_and_labels_and_save_predictions(
             raster_path,
             pos_labels_path,
             neg_labels_path,
             output_path=raster_path.parent.parent / "prediction.tif",
-            feature_type=FeatureType.FLAIR,
-            features_path=None,
-            compute_mode="normal",
-            chunks=None,
-            chunk_overlap=25,
+            feature_type=feature_type,
+            compute_mode=compute_mode,
+            chunks=chunk_size,
+            chunk_overlap=overlap_size,
         )
 
         # Add the new raster layer to QGIS
